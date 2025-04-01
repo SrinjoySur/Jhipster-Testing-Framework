01d0c3d7c69a1ed5f46204410667cdf4f653020d
Srinjoy Sur <srinjoy_sur@epam.com>
2025-04-01 11:35:37 +0530

    commit 2

--- src/test/java/com/epam/ui/tests/LoginTest.java
+++ src/test/java/com/epam/ui/tests/LoginTest.java
@@ -1,4 +1,4 @@
-package com.epam.ui.basetest.tests;
+package com.epam.ui.tests;
 import com.epam.ui.basetest.BaseTest;
 import com.epam.ui.basepages.BasePage;
 import com.epam.ui.factories.BrowserType;
@@ -6,10 +6,12 @@ import com.epam.ui.pages.HomePage;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+
 import static org.testng.Assert.assertEquals;
 
 public class LoginTest extends BaseTest {
-    private BasePage page;
+    private HomePage page;
     @Test(testName = "homePageRedirectionTest",description = "Redirects from HomePage to LoginPage")
     @Parameters("browser")
     void homePageRedirectionTest(BrowserType browserType){
@@ -18,9 +20,9 @@ public class LoginTest extends BaseTest {
         String actual= page.verifyTitle();
         assertEquals(actual,"Sign in");
     }
-    @Test(dependsOnMethods = "homePageRedirectionTest")
-    void validLoginTest(){
-        HashMap
-
-    }
+//    @Test(dependsOnMethods = "homePageRedirectionTest")
+//    void validLoginTest(){
+//        HashMap
+//
+//    }
 }
