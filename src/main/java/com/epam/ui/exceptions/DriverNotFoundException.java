6f4893ce7fe42a0b88cc328d0f48734a28730d40
Srinjoy Sur <srinjoy_sur@epam.com>
2025-04-04 17:58:29 +0530

    commit 1

--- src/main/java/com/epam/ui/exceptions/DriverNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.epam.ui.exceptions;
-
-public class DriverNotFoundException extends RuntimeException {
-    public DriverNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/epam/ui/factories/BrowserType.java
+++
@@ -1,8 +0,0 @@
-package com.epam.ui.factories;
-
-public enum BrowserType {
-    CHROME,
-    EDGE,
-    SAFARI,
-    FIREFOX
-}
